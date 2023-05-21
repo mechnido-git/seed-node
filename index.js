c5a4dd309131e32c149e072f464406fcdb48b019
mishalp <mishalclever@gmail.com>
2023-05-21 21:04:55 +0530

    update

--- index.js
+++ index.js
@@ -5,45 +5,65 @@ const stripe = require("stripe")(process.env.STRIPE_KEY);
 const express = require("express");
 const app = express();
 const cors = require("cors");
-const serviceAC = require('./cert.json')
+const serviceAC = require("./cert.json");
 
-const { initializeApp, applicationDefault, cert } = require('firebase-admin/app');
-const { getFirestore, Timestamp, FieldValue } = require('firebase-admin/firestore');
+const {
+  initializeApp,
+  applicationDefault,
+  cert,
+} = require("firebase-admin/app");
+const {
+  getFirestore,
+  Timestamp,
+  FieldValue,
+} = require("firebase-admin/firestore");
 initializeApp({
-  credential: cert(serviceAC)
+  credential: cert(serviceAC),
 });
 
 const db = getFirestore();
 
-const endpointSecret = 'whsec_3c789b0d4100b3998c434f0332c0e86be25c59bf3e314b728627cc3b01e8ea18';
+const endpointSecret = process.env.END_SECRET;
 
 app.use(cors());
-
 
 const courses = new Map([[0, { items: [20000, 35000, 45000] }]]);
 
 const YOUR_DOMAIN = "http://localhost:4242";
 
-const get = (index, id) => {
-    const item = courses.get(index);
-    console.log(item.items[id]);
-    console.log(id);
-    return item.items[id];
+const get = async (index, id) => {
+  const item = courses.get(index);
+  console.log("id ->"+id);
+  console.log(index);
+  const cityRef = db.collection("courses").doc(index);
+  const doc = await cityRef.get();
+  if (!doc.exists) {
+    console.log("No such document!");
+  } else {
+    console.log("Document data:", doc.data().fee[id].price);
   }
+  return parseInt(doc.data().fee[id].price*100);
+};
 
-  const read = async() => {
-   
-  } 
+const read = async (data) => {
+  console.log(data);
+  const course = db.collection('courses').doc(data.courseId);
+   const unionRes = await course.update({
+    enrolled: FieldValue.arrayUnion({userId: data.userId, payRange: data.range})
+   });
+};
 
-
-  app.post('/webhook', express.raw({type: 'application/json'}), (request, response) => {
+app.post(
+  "/webhook",
+  express.raw({ type: "application/json" }),
+  (request, response) => {
     let event = request.body;
     let data;
     // Only verify the event if you have an endpoint secret defined.
     // Otherwise use the basic event deserialized with JSON.parse
     if (endpointSecret) {
       // Get the signature sent by Stripe
-      const signature = request.headers['stripe-signature'];
+      const signature = request.headers["stripe-signature"];
       try {
         event = stripe.webhooks.constructEvent(
           request.body,
@@ -54,21 +74,25 @@ const get = (index, id) => {
         console.log(`⚠️  Webhook signature verification failed.`, err.message);
         return response.sendStatus(400);
       }
-     // data = request.body.data.object;
+      // data = request.body.data.object;
     }
-    
-  
+
     // Handle the event
     switch (event.type) {
-      case 'checkout.session.completed':
-        console.log('compleate');
-         stripe.customers.retrieve(event.data.object.customer).then(customer=>{
-          console.log(customer); read()}).catch(err=>console.log(err)) 
+      case "checkout.session.completed":
+        console.log("compleate");
+        stripe.customers
+          .retrieve(event.data.object.customer)
+          .then((customer) => {
+            console.log(customer);
+            read(customer.metadata);
+          })
+          .catch((err) => console.log(err));
         //console.log(event.data);
         // Then define and call a method to handle the successful payment intent.
         // handlePaymentIntentSucceeded(paymentIntent);
         break;
-      case 'payment_method.attached':
+      case "payment_method.attached":
         const paymentMethod = event.data.object;
         // Then define and call a method to handle the successful attachment of a PaymentMethod.
         // handlePaymentMethodAttached(paymentMethod);
@@ -77,23 +101,23 @@ const get = (index, id) => {
         // Unexpected event type
         console.log(`Unhandled event type ${event.type}.`);
     }
-  
+
     // Return a 200 response to acknowledge receipt of the event
     response.send();
-  });
+  }
+);
 
-  app.use(express.json());
-
+app.use(express.json());
 
 app.post("/create-checkout-session", async (req, res) => {
   try {
-
     const customer = await stripe.customers.create({
       metadata: {
         userId: req.body.userId,
-        courseId: req.body.id
-      }
-    })
+        courseId: req.body.id,
+        range: req.body.range
+      },
+    });
 
     const session = await stripe.checkout.sessions.create({
       line_items: [
@@ -103,7 +127,7 @@ app.post("/create-checkout-session", async (req, res) => {
             product_data: {
               name: req.body.name,
             },
-            unit_amount: get(req.body.id, req.body.range),
+            unit_amount: await get(req.body.id, req.body.range),
           },
           // Provide the exact Price ID (for example, pr_1234) of the product you want to sell
           quantity: 1,
@@ -111,7 +135,7 @@ app.post("/create-checkout-session", async (req, res) => {
       ],
       customer: customer.id,
       mode: "payment",
-      success_url: `http://localhost:3000?success=true`,
+      success_url: `http://localhost:3000/menu/courses`,
       cancel_url: `http://localhost:3000?canceled=true`,
     });
     res.status(200).json({ url: session.url });
@@ -120,8 +144,5 @@ app.post("/create-checkout-session", async (req, res) => {
   }
   //res.redirect(303, session.url);
 });
-
-
-
 
 app.listen(4242, () => console.log("Running on port 4242"));
